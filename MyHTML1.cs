7cc80c344688c61e281844a52f776a42f0a7dfa1
Sn0wCh1ld <nojailbreakapps@outlook.com>
2015-01-11 11:58:24 -0500

    test commit

---
+++ MyHTML1.cs
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>New Smallcut</title>
+  </head>
+  <body>
+    <div style="text-align:center">
+    	This is a webpage<br>
+      	But no matter what, I am King
+      	<p><a href=http://www.kiaye.org/>KIAYEorg</a></p>
+  		<br>
+  		<img src=KingSn0w.png width="100" height="100" alt=“King Sn0wCh1ld’s logo”>
+  		<form action="upload.php" method="post" enctype="multipart/form-data">
+    	Select game to upload (must be in .swf format):
+    	<input type="file" name="fileToUpload" id="fileToUpload">
+    	<input type="submit" value="Upload Image" name="submit">
+		</form>
+    </div>
+  </body>
+</html>
---
+++ upload.php
@@ -0,0 +1,29 @@
+<?php
+$target_dir = uploads;
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+    if($check !== false) {
+        echo "File is an image - " . $check["mime"] . ".";
+        $uploadOk = 1;
+    } else {
+        echo "File is not an image.";
+        $uploadOk = 0;
+    }
+}
+
+// Check if file already exists
+if (file_exists($target_file)) {
+    echo "Sorry, file already exists.";
+    $uploadOk = 0;
+}
+
+// Allow certain file formats
+if($imageFileType != "swf") {
+    echo "Sorry, only .swf files are allowed.";
+    $uploadOk = 0;
+}
+?>
